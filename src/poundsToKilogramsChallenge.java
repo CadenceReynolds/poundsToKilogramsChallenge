08c722174a741ddb5dcde9c72dfdda3bcbfbff61
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-07 13:07:44 -0400

    Finished Challenge

--- src/poundsToKilogramsChallenge.java
+++ src/poundsToKilogramsChallenge.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class poundsToKilogramsChallenge {
+public class poundsToKilogramsChallenge {
+    public static void main(String[] args) {
+        var pounds = 200;
+        var kilograms = pounds * 0.45359237;
+        System.out.println(kilograms);
+
+    }
+
 }
