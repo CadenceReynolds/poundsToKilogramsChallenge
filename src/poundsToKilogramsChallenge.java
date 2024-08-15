89d72ff344a05a9cf37d1a228f4a5afe156e570b
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-15 12:58:13 -0400

    Finished Challenge corrected

--- src/poundsToKilogramsChallenge.java
+++ src/poundsToKilogramsChallenge.java
@@ -1,9 +1,22 @@
 public class poundsToKilogramsChallenge {
     public static void main(String[] args) {
-        double pounds = 200;
-        double kilograms = pounds * 0.45359237;
-        System.out.println(kilograms);
+        System.out.println(toMilersPerHour(1.5));
+        System.out.println(toMilersPerHour(10.25));
+        System.out.println(toMilersPerHour(-5.6));
+        System.out.println(toMilersPerHour(25.42));
+        System.out.println(toMilersPerHour(75.114));
+
 
     }
 
+    public static long toMilersPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            double milesPerHour = kilometersPerHour / 1.609;
+            return Math.round(milesPerHour);
+        }
+
+
+    }
 }
